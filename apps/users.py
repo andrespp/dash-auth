78f1821e96e66e6f1267ccc98f79004063952d9f
Andre Pereira <andrespp@gmail.com>
2021-04-19 00:20:29 -0300

    feat: add user creation interface

--- apps/users.py
+++ apps/users.py
@@ -14,25 +14,94 @@ from models import User
 def layout():
     """Define dashboard layout
     """
+    # New user Form
+    signup_form = [
+
+        dbc.FormGroup([
+            dbc.Label('Name', html_for='name'),
+            dbc.Input(type='text', id='name',
+                      placeholder='First and Last Name'),
+        ]),
+
+        dbc.FormGroup([
+            dbc.Label('Email', html_for='signup-email'),
+            dbc.Input(type='email', id='email', placeholder='Enter email'),
+        ]),
+
+        dbc.FormGroup([
+            dbc.Label('Password', html_for='password'),
+            dbc.Input(type='password',
+                      id='password',
+                      placeholder='Enter password'
+                     ),
+        ]),
+
+        dbc.FormGroup([
+            dbc.Label('Password (Confirm)', html_for='password2'),
+            dbc.Input(type='password',
+                      id='password2',
+                      placeholder='Confirm password'
+                     ),
+        ]),
+
+    ]
+    # modal
+    modal = html.Div(
+        [
+            dbc.Modal(
+                [
+                    dbc.ModalHeader('Add user'),
+                    dbc.ModalBody(signup_form),
+                    dbc.ModalFooter([
+                        dbc.Button('Close', id='close', className='ml-auto'),
+                        dbc.Button('Create new user',
+                                   id='signup-button',
+                                   color='primary',
+                                   className='ml-1'
+                                  ),
+
+                    ]),
+                ],
+                id='modal',
+            ),
+        ]
+    )
+
     # Page Layout
     layout = [
 
-            # Title Row
-            dbc.Row([
-                dbc.Col(html.P('System Users',
-                               className='p-5 m-0 text-center'
-                              ),
-                        width={'size':12, 'offset':0},
-                        className='px-3',
-                ),
-            ]),
+        # Title Row
+        dbc.Row([
 
-            # Graph Row
-            dbc.Row([
-                dbc.Col(
-                    html.Div(id='table1'),
-                ),
-            ]),
+            dbc.Col(html.H5('System Users',
+                           className='py-3 m-0 text-left'
+                          ),
+                    width={'size':4, 'offset':0},
+                    className='px-3',
+            ),
+
+            dbc.Col(dbc.Button('Add User',
+                               id='open',
+                               color='secondary',
+                               size='sm',
+                               className='m-0 float-right',
+                              ),
+                    width={'size':4, 'offset':0},
+                    className='py-3',
+                   ),
+        ],
+            justify='between',
+        ),
+
+        # Graph Row
+        dbc.Row([
+            dbc.Col(
+                html.Div(id='table1'),
+            ),
+        ]),
+
+        # Modal Row
+        dbc.Row(modal),
 
     ]
 
@@ -51,7 +120,7 @@ def update_table1(active_cell):
     df = lookup_data()
 
     table = dash_table.DataTable(
-        columns=[{"name": i, "id": i,
+        columns=[{'name': i, 'id': i,
                   'type':'numeric',
                  } for i in df.columns],
         data=df.to_dict('records'),
@@ -62,15 +131,15 @@ def update_table1(active_cell):
                     'maxWidth': 0
                    },
         style_cell_conditional=[
-            {'if': {'column_id': 'id'},
-             'width': '10%',
+            {'if': {'column_id': 'uid'},
+             'width': '5%',
             },
             {'if': {'column_id': 'name'},
              'width': '20%',
             },
-            #{'if': {'column_id': 'email'},
-            # 'width': '50%',
-            #},
+            {'if': {'column_id': 'active'},
+             'width': '10%',
+            },
         ],
 
         tooltip_data=[
@@ -81,8 +150,18 @@ def update_table1(active_cell):
         ],
         tooltip_duration=None,
     ),
-
     return table
+
+@app.callback(
+    Output('modal', 'is_open'),
+     Input('open', 'n_clicks'),
+     Input('close', 'n_clicks'),
+     State('modal', 'is_open'),
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
 
 ###############################################################################
 # Data lookup functions
@@ -96,6 +175,7 @@ def lookup_data():
     email=[]
     created=[]
     modified=[]
+    active=[]
 
     users = User.query.all()
 
@@ -105,6 +185,7 @@ def lookup_data():
         email.append(i.email)
         created.append(i.created)
         modified.append(i.modified)
+        active.append(i.active)
 
     df = pd.DataFrame(
         {'uid':uid,
@@ -112,6 +193,7 @@ def lookup_data():
          'email':email,
          'created':created,
          'modified':modified,
+         'active':active,
         }
     )
 
@@ -119,5 +201,6 @@ def lookup_data():
         lambda x: x.strftime('%Y-%m-%d, %H:%M %Z') if x else None)
     df['modified'] = df['modified'].apply(
         lambda x: x.strftime('%Y-%m-%d, %H:%M %Z') if x else None)
+    df['active'] = df['active'].apply(lambda x: 'X' if x else None)
 
     return df
