a8133fc58a2fe2cf05ceef6628c0e6f4f777fb16
Andre Pereira <andrespp@gmail.com>
2021-04-20 21:21:25 -0300

    feat: implement account creation

--- apps/users.py
+++ apps/users.py
@@ -1,11 +1,17 @@
 """users.py
 """
+import re
 import pandas as pd
+import dash
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_table
+import datetime as dt
+import traceback
+from sqlalchemy.exc import IntegrityError
 from dash.dependencies import Input, Output, State
-from app import app, DWO
+from werkzeug.security import generate_password_hash
+from app import app, db, DWO
 from models import User
 
 ###############################################################################
@@ -16,6 +22,8 @@ def layout():
     """
     # New user Form
     signup_form = [
+
+        html.Div(id='signup-alert'),
 
         dbc.FormGroup([
             dbc.Label('Name', html_for='name'),
@@ -29,9 +37,9 @@ def layout():
         ]),
 
         dbc.FormGroup([
-            dbc.Label('Password', html_for='password'),
+            dbc.Label('Password', html_for='password1'),
             dbc.Input(type='password',
-                      id='password',
+                      id='password1',
                       placeholder='Enter password'
                      ),
         ]),
@@ -50,7 +58,7 @@ def layout():
         [
             dbc.Modal(
                 [
-                    dbc.ModalHeader('Add user'),
+                    dbc.ModalHeader(['Add user', ]),
                     dbc.ModalBody(signup_form),
                     dbc.ModalFooter([
                         dbc.Button('Close', id='close', className='ml-auto'),
@@ -159,9 +167,166 @@ def update_table1(active_cell):
      State('modal', 'is_open'),
 )
 def toggle_modal(n1, n2, is_open):
+    """toggle_modal()
+    """
     if n1 or n2:
         return not is_open
     return is_open
+
+@app.callback(
+    Output('signup-alert','children'),
+    Input('signup-button','n_clicks'),
+    State('name','value'),
+    State('email','value'),
+    State('password1','value'),
+    State('password2','value'),
+)
+def create_user_btn(btn, name, email, p1, p2):
+    """create_user()
+    """
+    user = {'name':None, 'email':None, 'password':None, 'active':False}
+
+    if not btn:
+        return None
+
+    # Check Name
+    if not name:
+        return dbc.Alert('Name is empty',
+                         dismissable=True, color='danger'),
+    elif len(name.strip()) < 3:
+        return dbc.Alert('User name too short.',
+                         dismissable=True, color='danger'),
+    elif not name.replace(" ", "").isalpha():
+        return dbc.Alert('User name contains invalid characters.',
+                         dismissable=True, color='danger'),
+    else:
+        user['name'] = name.strip()
+
+    # Check email
+    pattern = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+    if not email:
+        return dbc.Alert('Email is empty',
+                         dismissable=True, color='danger'),
+
+    elif re.match(pattern, email):
+        user['email'] = email
+    else:
+        return dbc.Alert('Invalid email',
+                         dismissable=True, color='danger'),
+
+    # Check password
+    if not p1:
+        return dbc.Alert('Password is empty',
+                         dismissable=True, color='danger'),
+    if len(p1) < 6:
+        return dbc.Alert('The password must be at least 6 characters long.',
+                         dismissable=True, color='danger'),
+    elif not p1==p2:
+        return dbc.Alert('Passwords don\'t match.',
+                         dismissable=True, color='danger'),
+    else:
+        user['password'] = p1
+
+    # Create account
+    try:
+        usr = User(email=user['email'],
+                    name=user['name'],
+                    password=generate_password_hash(user['password'],
+                                                    method='sha256'
+                                                   ),
+                    created=dt.datetime.today(),
+                    active=True,
+                   )
+
+        db.create_all()
+        db.session.add(usr)
+        db.session.commit()
+        return dbc.Alert('Acount created!', dismissable=True, color='success'),
+    except IntegrityError:
+        db.session.rollback()
+        return dbc.Alert(f'Error creating account! User already exists',
+                         dismissable=True, color='danger'),
+    except Exception as e:
+        db.session.rollback()
+        traceback.print_exc()
+        return dbc.Alert(f'Error creating account: {e}',
+                         dismissable=True, color='danger'),
+
+
+@app.callback(
+    Output('name', 'invalid'),
+    Output('name', 'title'),
+    Input('name','value'),
+)
+def validate_signup_name(name):
+    """Validade signup form
+    """
+    # blank cell is not invalid
+    if not name:
+        return False, None
+
+    # Check Name
+    name = name.strip()
+    if len(name) < 3:
+        return True, 'Name too short.'
+    elif not name.replace(" ", "").isalpha():
+        return True, 'Invalid characters.'
+    else:
+        return False, None
+
+@app.callback(
+    Output('email', 'invalid'),
+    Output('email', 'title'),
+    Input('email','value'),
+)
+def validate_signup_email(email):
+    """Validade signup form
+    """
+    # blank cell is not invalid
+    if not email:
+        return False, None
+
+    # Check email
+    pattern = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+    if(re.match(pattern, email)):
+        return False, None
+    else:
+        return True, 'Invalid email'
+
+@app.callback(
+    Output('password1', 'invalid'),
+    Output('password2', 'invalid'),
+    Output('password1', 'title'),
+    Output('password2', 'title'),
+    Input('password1','value'),
+    Input('password2','value'),
+    State('password1','invalid'),
+    State('password2','invalid'),
+)
+def validate_signup_password(p1, p2, sp1, sp2):
+    """Validade signup form
+    """
+    invalid = {'p1':sp1, 'p2':sp2}
+    title = {'p1':None, 'p2':None}
+
+    if p1:
+        if len(p1) < 6:
+            invalid['p1'] = True
+            title['p1'] = 'The password must be at least 6 characters long.'
+        else:
+            invalid['p1'] = False
+
+    if p2:
+        if not p1:
+            invalid['p2'] = True
+            title['p2'] = 'Fill password field.'
+        elif not p1==p2:
+            invalid['p2'] = True
+            title['p2'] = 'Passwords don\'t match.'
+        else:
+            invalid['p2'] = False
+
+    return invalid['p1'], invalid['p2'], title['p1'], title['p2']
 
 ###############################################################################
 # Data lookup functions
